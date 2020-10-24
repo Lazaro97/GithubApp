11a25800a8746067a505b5791768661f4e55fa33
Lazaro Ambrosio <LazaroAmbrosio97@gmail.com>
2020-10-24 15:51:56 -0400

    App Navigation Setup - UITabbar Controller & UINavigationController

---
+++ GithubApp/FavoritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  GithubApp
+//
+//  Created by Lazaro Ambrosio on 10/24/20.
+//  Copyright © 2020 Lazaro Ambrosio. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ GithubApp/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GithubApp
+//
+//  Created by Lazaro Ambrosio on 10/24/20.
+//  Copyright © 2020 Lazaro Ambrosio. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
