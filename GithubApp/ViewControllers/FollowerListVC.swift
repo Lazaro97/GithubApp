cf565e7c2c8dfcbd5f52f1168bf6b0f460f88242
Lazaro Ambrosio <LazaroAmbrosio97@gmail.com>
2020-11-03 01:21:47 -0500

    UICollectionView-  Intital Setup

--- GithubApp/ViewControllers/FollowerListVC.swift
+++ GithubApp/ViewControllers/FollowerListVC.swift
@@ -11,13 +11,31 @@ import UIKit
 class FollowerListVC: UIViewController {
    
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.resueID)
+    }
+    
+    func getFollowers() {
         NetworkManager.shared.getFollower(for: username, page: 1) { result in
         
         switch result {
