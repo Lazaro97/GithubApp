c849f16422ff2d8aa52ed2d430a456636be8f231
Lazaro Ambrosio <LazaroAmbrosio97@gmail.com>
2020-11-05 13:29:06 -0500

    UserInfoVC Cleanup & Setup

--- GithubApp/ViewControllers/UserInfoVC.swift
+++ GithubApp/ViewControllers/UserInfoVC.swift
@@ -11,32 +11,43 @@ import UIKit
 class UserInfoVC: UIViewController {
     
     let headerView = UIView()
-    
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    var itemViews: [UIView] = [ ]
     var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        print(username!)
+        configureViewController()
+        layoutUI()
+        getGithubUserInfomarion()
+    }
+    
+   func getGithubUserInfomarion() {
+          NetworkManager.shared.getUserInformation(for: username) { [weak self] result in
+                     guard let self = self else {return}
+                     
+                     switch result {
+                     case .success(let user):
+                     //Here we add the headerVC
+                         DispatchQueue.main.async {
+                         self.add(childVC: GFUserHeaderVC(user: user), to: self.headerView)
+
+                         }
+                     case .failure(let error):
+                         self.presentGFAlertOnMainTread(title: "Something went wrong", message: error.rawValue, buttonTitle: "okay")
+                     }
+                 }
+      }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVCView))
         navigationItem.rightBarButtonItem = doneBtn
-        print(username!)
-        layoutUI()
-
-        NetworkManager.shared.getUserInformation(for: username) { [weak self] result in
-            guard let self = self else {return}
-            
-            switch result {
-            case .success(let user):
-            //Here we add the headerVC
-                DispatchQueue.main.async {
-                self.add(childVC: GFUserHeaderVC(user: user), to: self.headerView)
-
-                }
-            case .failure(let error):
-                self.presentGFAlertOnMainTread(title: "Something went wrong", message: error.rawValue, buttonTitle: "okay")
-            }
-        }
     }
+    
     
     @objc func dismissVCView() {
         dismiss(animated: true)
@@ -44,15 +55,44 @@ class UserInfoVC: UIViewController {
     
     
     func layoutUI() {
+        
+        ///Header View
         view.addSubview(headerView)
+        
         headerView.translatesAutoresizingMaskIntoConstraints = false
         headerView.backgroundColor = .systemPink
+        
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             headerView.heightAnchor.constraint(equalToConstant: 180)
         ])
+        
+        ///ItemViewOne
+        view.addSubview(itemViewOne)
+        itemViewOne.backgroundColor = .systemPink
+
+        itemViewOne.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 20),
+            itemViewOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            itemViewOne.heightAnchor.constraint(equalToConstant: 140)
+            ,])
+        
+        ///ItemViewTwo
+        view.addSubview(itemViewTwo)
+        itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
+        itemViewTwo.backgroundColor = .systemPink
+
+
+        NSLayoutConstraint.activate([
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: 20),
+            itemViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: 140)
+        ,])
     }
     
     func add(childVC: UIViewController, to containerView: UIView) {
@@ -61,4 +101,6 @@ class UserInfoVC: UIViewController {
         childVC.view.frame = containerView.bounds
         childVC.didMove(toParent: self)
     }
+    
+    
 }
