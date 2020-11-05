76b5f4c6462fd3d5a9259ada62b72b3137a6409a
Lazaro Ambrosio <LazaroAmbrosio97@gmail.com>
2020-11-05 14:40:27 -0500

    ItemInfoVC - SuperClass

---
+++ GithubApp/ViewControllers/GFItemInfoVCViewController.swift
@@ -0,0 +1,61 @@
+//
+//  GFItemInfoVCViewController.swift
+//  GithubApp
+//
+//  Created by Lazaro Ambrosio on 11/5/20.
+//  Copyright © 2020 Lazaro Ambrosio. All rights reserved.
+//
+
+import UIKit
+
+class GFItemInfoVCViewController: UIViewController {
+
+    let stackView = UIStackView()
+    let infoItemViewOne = GFItemInfoVIew()
+    let infoItemViewTwo = GFItemInfoVIew()
+    let actionButton = GFButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    //Stackview
+    private func configureStackView() {
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(infoItemViewOne)
+        stackView.addArrangedSubview(infoItemViewTwo)
+    }
+    
+    
+    //StackView Constraints and button 
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}
