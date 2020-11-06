a9bb925b0cc5fb7f1a47b9a8ba6fd9921f6358cb
Lazaro Ambrosio <LazaroAmbrosio97@gmail.com>
2020-11-06 15:03:07 -0500

    Persistance Manager

---
+++ GithubApp/Managers/PersistanceManager.swift
@@ -0,0 +1,77 @@
+//
+//  PersistanceManager.swift
+//  GithubApp
+//
+//  Created by Lazaro Ambrosio on 11/6/20.
+//  Copyright © 2020 Lazaro Ambrosio. All rights reserved.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+
+enum PersistanceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    static func upateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping(GFError?) -> Void ) {
+        retrieveFavorites { result in
+            switch result {
+            case.success(let favorites):
+                var retrieveFavorties = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrieveFavorties.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                    return
+                }
+                    
+                retrieveFavorties.append(favorite)
+               
+                case .remove:
+                    retrieveFavorties.removeAll { $0.login == favorite.login}
+                    return
+                }
+                
+                completed(save(favorites: favorites))
+                
+            case.failure(let error) :
+                completed(error)
+            }
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>)-> Void){
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFav))
+        }
+    }
+
+static func save(favorites: [Follower]) -> GFError? {
+    do {
+        let encoder = JSONEncoder()
+        let encodedFavorites = try encoder.encode(favorites)
+        defaults.set(encodedFavorites, forKey: Keys.favorites)
+        return nil
+    }
+    catch {
+        return .unableToFav
+        }
+    }
+}
--- GithubApp/Utilities/ErrorMessage.swift
+++ GithubApp/Utilities/ErrorMessage.swift
@@ -13,4 +13,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request. Please check your internet connection"
     case invalidResponse =  "Invalid response from the server. Please try again."
     case invalidData =       "The data recieved from the server was invalid.Please try again."
+    case unableToFav = "There was an error. Please try again"
+    case alreadyInFavorites = "Already favorited"
 }
